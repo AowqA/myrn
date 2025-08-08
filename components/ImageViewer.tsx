6efaf9f3786429435bc618e128e377aa03428f0f
AowqA <1651057012@qq.com>
2025-08-08 23:09:03 +0800

    统一组件缩进风格并修复按钮默认点击事件

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -2,19 +2,19 @@ import { Image } from 'expo-image';
 import { ImageSourcePropType, StyleSheet } from 'react-native';
 
 type Props = {
-  imgSource: ImageSourcePropType;
-  selectedImage?: string;
+    imgSource: ImageSourcePropType;
+    selectedImage?: string;
 };
 
 export default function ImageViewer({ imgSource, selectedImage }: Props) {
-  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
-  return <Image source={imageSource} style={styles.image} />;
+    const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+    return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
 });
