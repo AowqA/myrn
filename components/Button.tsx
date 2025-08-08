7480e9c8e566a0d65a57feae72bac22f6a58f5bb
AowqA <1651057012@qq.com>
2025-08-08 23:00:21 +0800

    实现图片选择功能并优化按钮组件

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,20 +4,30 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 type Props = {
   label: string;
   theme?: 'primary';
+  onPress?: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === 'primary') {
     return (
-      <View
+      <View 
         style={[
-          styles.buttonContainer,
-          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
-        ]}>
+            styles.buttonContainer, 
+            {
+                borderColor: '#ffd33d',
+                borderWidth: 4, 
+                borderRadius: 10
+            }]}>
         <Pressable
           style={[styles.button, { backgroundColor: '#fff' }]}
-          onPress={() => alert('You pressed a button.')}>
-          <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+          onPress={onPress}
+        >
+            <FontAwesome 
+                name="picture-o" 
+                size={18} 
+                color="#25292e" 
+                style={styles.buttonIcon} 
+            />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
       </View>
@@ -25,7 +35,10 @@ export default function Button({ label, theme }: Props) {
   }  
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+        <Pressable 
+            style={styles.button} 
+            onPress={() => alert('You pressed a button.')}
+        >
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
