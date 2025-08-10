735b61cff07a4c87c94e6cc244c985bb7aecb59c
AowqA <1651057012@qq.com>
2025-08-10 15:01:37 +0800

    优化 EmojiSticker 组件，添加拖动手势支持并更新样式；更新依赖项以包含 expo-media-library 和 react-native-view-shot

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,43 +1,59 @@
-// 导入所需的组件和库
-import { ImageSourcePropType, View } from 'react-native';
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
+import { ImageSourcePropType } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
-// 定义组件的属性类型
 type Props = {
-  imageSize: number;  // 表情图片的尺寸
-  stickerSource: ImageSourcePropType;  // 表情图片资源
+  imageSize: number;
+  stickerSource: ImageSourcePropType;
 };
 
-// 导出默认的 EmojiSticker 组件，接收 imageSize 和 stickerSource 作为属性
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
     .onStart(() => {
-      if(scaleImage.value !== imageSize*2){
-        scaleImage.value = scaleImage.value*2;
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
       } else {
-        scaleImage.value = Math.round(scaleImage.value/2)
+        scaleImage.value = Math.round(scaleImage.value / 2);
       }
     });
+
   const imageStyle = useAnimatedStyle(() => {
     return {
       width: withSpring(scaleImage.value),
       height: withSpring(scaleImage.value),
     };
   });
-  // 渲染组件 UI
+
+  const drag = Gesture.Pan().onChange(event => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: translateX.value,},
+        {translateY: translateY.value,},
+      ],
+    };
+  });
+
   return (
-    // 容器视图，用于设置表情图片的位置
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}  
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
